9c2ca2de9230c259019db580b04849fcef0e0356
harshasukhdeve <harshasukhdeve0903@gmail.com>
2020-06-13 17:05:50 +0530

    FSJ0420A115

---
+++ directive-ngfor-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'directive-ngfor-app';
+  productList : Array<string> = [
+    'phone',
+    'Galaxy 9.0',
+    'BlackBerrry 10z',
+    'Mi'
+  ]
+}
